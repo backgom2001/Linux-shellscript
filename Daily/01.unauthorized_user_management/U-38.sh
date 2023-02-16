5cf55d8599391d9a57275514cafffb2b3ebca9fe
backgom2001 <120619854+backgom2001@users.noreply.github.com>
2023-02-16 13:32:28 +0900

    Update U-38.sh

--- Daily/01.unauthorized_user_management/U-38.sh
+++ Daily/01.unauthorized_user_management/U-38.sh
@@ -1,58 +1,23 @@
 #!/bin/bash
 
+# log파일로 해당 진단 내용 파일 생성
 . function.sh
 TMP1=`SCRIPTNAME`.log
-
 > $TMP1
 
-
 BAR
-
-CODE [U-38] 웹서비스 불필요한 파일 제거
+CODE [ U-38 ] 웹서비스 불필요한 파일 제거
 cat << EOF >> $RESULT
-[양호]: r 계열 서비스가 비활성화 되어 있는 경우
-[취약]: r 계열 서비스가 활성화 되어 있는 경우
+[ 양호 ] : 기본으로 생성되는 불필요한 파일 및 디렉터리가 제거되어 있는 경우
+[ 취약 ] : 기본으로 생성되는 불필요한 파일 및 디렉터리가 제거되지 않은 경우
 EOF
-
 BAR
 
+INFO $TMP1 파일을 확인하여 불필요한 파일 여부를 확인해야 합니다.
 
-TMP2=/tmp/tmp2
-
-ls -1 /etc/xinetd.d | egrep '(rsh|rlogin|rexec)' > $TMP2
-
-if [ -s $TMP2 ] ; then
-
-WARN 'rCMD(rlogin,rsh,rcp) 서비스가 존재합니다.'
-
-for i in `cat $TMP2`
-
-do
-
-RES=`cat /etc/xinetd.d/$i | egrep -v '^#' \
-
-| egrep disable \
-
-| awk '{print $3}'`
-
-if [ $RES = 'yes' ] ; then
-
-OK $i 서비스가 활성화 되어 있지 않습니다.
-
-else
-
-WARN $i 서비스가 활성화 되어 있습니다.
-
-fi
-
-done
-
-else
-
-OK 'rCMD(rlogin,rsh,rcp) 서비스가 존재하지 않습니다.'
-
-fi
+find / -name manual > $TMP1
+ls -l /etc/httpd/manual >> $TMP1
+ls –l /var/www/manual >> $TMP1
 
 cat $RESULT
-
-echo ; echo
+echo;
