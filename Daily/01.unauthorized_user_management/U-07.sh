7218c4b6c2635418e4d7255b07fbe840c7ab589f
backgom2001 <120619854+backgom2001@users.noreply.github.com>
2023-02-16 13:28:40 +0900

    Update U-07.sh
    7번코드도 잘못된 코드 가져와서 전부수정했습니다ㅜ

--- Daily/01.unauthorized_user_management/U-07.sh
+++ Daily/01.unauthorized_user_management/U-07.sh
@@ -2,55 +2,39 @@
 
 . function.sh
 
-TMP1=`SCRIPTNAME`.log
-
-> $TMP1
-
-TMP2=$(mktemp)
-
-
 BAR
-
-CODE [U-07] 패스워드 최소 길이 설정
-
+CODE [U-07] /etc/passwd 파일 소유자 및 권한 설정
 cat << EOF >> $RESULT
-
-[양호]: 암호의 최소 길이가 8글자 이상
-
-[취약]: 암호의 최소 길이가 7글자 미만
-
+[양호]:  /etc/passwd 파일의 소유자가 root이고, 권한이 644 이하인 경우
+[취약]:  /etc/passwd 파일의 소유자가 root가 아니거나, 권한이 644 이하가 아닌 경우
 EOF
-
 BAR
 
-LOGINDEFSFILE=/etc/login.defs \
-grep PASS_MIN_LEN $LOGINDEFSFILE | egrep -v '^#|^$' >> $TMP2
-
-NUM=$(cat $TMP2 | awk '{print $2}')
-
-if [ $NUM -ge 7 ] ; then
-
-OK 암호의 최소 길이가 8글자 이상입니다.
-
-else
-
-WARN 암호의 최소 길이가 7글자 이하입니다.
-
-INFO $TMP1 파일을 참고하십시오.
-
-echo "=============================================================" >> $TMP1
-
-echo "1. /etc/login.defs 파일 내용 중 PASS_MIN_LEN 설정 부분입니다." >> $TMP1
-
-echo "" >> $TMP1
-
-cat $TMP2 >> $TMP1
-
-echo "=============================================================" >> $TMP1
-
+CHECK=0
+ls /etc/passwd > /dev/null 2>&1
+if [ $? = 0 ] ; then
+        CHECK=1
 fi
 
+if [ $CHECK != 0 ] ; then
+        CHOWN1=`ls -l /etc/passwd | awk '{print $3}'`
+        if [ $CHOWN1 = root ] ; then
+                OK /etc/passwd 파일이 존재하며 소유자는 root입니다.
+        else
+                WARN /etc/passwd 파일이 존재하지만 소유자는 root가 아닙니다.
+        fi
+elif [ $CHECK = 0 ] ; then
+        INFO /etc/passwd 파일이 존재하지 않습니다.
+fi
+
+if [ $CHECK = 1 ] ; then
+        find /etc/passwd -type f -perm -644 -ls | grep -v 'rw-r--r--' >/dev/null 2>&1
+        if [ $? = 0 ] ; then
+                WARN passwd 파일의 권한이 `ls -l /etc/passwd | awk '{print $1}'`로 설정되어 644보다 높습니다.
+        else
+                OK passwd 파일의 권한이 644이하 입니다.
+        fi
+fi
 
 cat $RESULT
 
-echo ; echo
